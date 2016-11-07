d76807ecad98112532625e083ea022869a9e7da1
kellie <kellie@theironyard.com>
2016-11-07 13:45:38 -0700

    hello world commit

---
+++ day1/animals/HelloWorld.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class HelloWorld{
+
+	
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+		Random random = new Random();
+		System.out.println(random.nextInt());
+
+
+	}
+}
